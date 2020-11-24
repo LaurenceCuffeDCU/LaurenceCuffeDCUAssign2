5b94e37f0da55f9ea7dbd2ba89d381f97c6e82a0
LaurenceCuffeDCU <Laurence.cuffe2@mail.dcu.ie>
2020-11-24 21:28:29 +0000

    Added some debug code, and set one debug message to fire when cammera ID is set. Also put in some prototype JavaDoc

--- app/src/main/java/com/example/laurencecuffeassignment2/MainActivity.java
+++ app/src/main/java/com/example/laurencecuffeassignment2/MainActivity.java
@@ -5,20 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    // making picture ID
+    // debug Tags
+    private static final String PTAG = "OpenCameraActive";
+    private static final String VTAG = "GalleryActive";
+    private static final String EMTAG = "EmailPrepActive";
+
+    // making picture ID Just an Int
     private static final int pic_id = 120;
     // defining a variable for the view object textView
     protected TextView camera_open_id;
 
+    /**
+     * @author Laurence Cuffe
+     * @version 0.8
+     * Includes a working Implied intent for Open Camera trigering off the first textView
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         camera_open_id =  findViewById(R.id.textView);
+        Log.i(PTAG,"Camera_open_Id set ");
         camera_open_id.setOnClickListener(new View.OnClickListener() {
 
             @Override
