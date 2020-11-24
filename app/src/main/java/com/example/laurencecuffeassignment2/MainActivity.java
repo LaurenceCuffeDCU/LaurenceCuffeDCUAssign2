97a27bd576fc0d986b35e79848d0bb8c2fe6614c
LaurenceCuffeDCU <Laurence.cuffe2@mail.dcu.ie>
2020-11-24 20:53:03 +0000

    This Commit has the Implicit intent, Open Camera working. I had previously got a version of the camera specific code working based on code from tutorials point, using a button. That code was simplified, and converted to trigering off a text view and tested, before the relevent parts were transfered into the main program here. The other code is in Cammera Stub

--- app/src/main/java/com/example/laurencecuffeassignment2/MainActivity.java
+++ app/src/main/java/com/example/laurencecuffeassignment2/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.laurencecuffeassignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // making picture ID
+    private static final int pic_id = 120;
+    // defining a variable for the view object textView
+    protected TextView camera_open_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        camera_open_id =  findViewById(R.id.textView);
+        camera_open_id.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent camera_intent
+                        = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // Start the activity with camera_intent,
+                // and request pic id
+                startActivityForResult(camera_intent, pic_id);
+            }
+        });
     }
 }
