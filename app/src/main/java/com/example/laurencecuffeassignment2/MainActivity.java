7e37aa72bc1bd32de9a3591136d131363f345604
LaurenceCuffeDCU <Laurence.cuffe2@mail.dcu.ie>
2020-11-24 22:42:36 +0000

    Added code for the Gallery View implicit Intent, and updated the JavaDoc, puting in a link to the main source I used. Tested code stand alone first for this intent and then brought it into the assignment, again changing a button into a textview as trigger

--- app/src/main/java/com/example/laurencecuffeassignment2/MainActivity.java
+++ app/src/main/java/com/example/laurencecuffeassignment2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.laurencecuffeassignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -19,11 +20,16 @@ public class MainActivity extends AppCompatActivity {
     private static final int pic_id = 120;
     // defining a variable for the view object textView
     protected TextView camera_open_id;
+    protected TextView picPic;
+    Uri imageUri;
 
     /**
      * @author Laurence Cuffe
      * @version 0.8
      * Includes a working Implied intent for Open Camera trigering off the first textView
+     * includes a working Implied intent for View Picture, This code is very relient on an example
+     * from https://www.tutorialspoint.com/how-to-pick-an-image-from-image-gallery-in-android
+     * @see <a href="https://www.tutorialspoint.com/how-to-pick-an-image-from-image-gallery-in-android">https://www.tutorialspoint.com/how-to-pick-an-image-from-image-gallery-in-android</a>
      * @param savedInstanceState
      */
     @Override
@@ -31,7 +37,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         camera_open_id =  findViewById(R.id.textView);
+        picPic = findViewById(R.id.textView2);
+
         Log.i(PTAG,"Camera_open_Id set ");
+        picPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+
+
+        });
         camera_open_id.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -43,5 +59,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(camera_intent, pic_id);
             }
         });
+
     }
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, pic_id);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == pic_id){
+            imageUri = data.getData();
+        }
+    }
+
 }
