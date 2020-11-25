9626446b8b4e7befb84ce4411364c77069a71cdd
LaurenceCuffeDCU <Laurence.cuffe2@mail.dcu.ie>
2020-11-25 17:43:28 +0000

    Now building the explicit intent for the email. I have got an email invocation running in project firstEmail, and as a fist step want to invoke the second screen. This commit just contains code to make the third button active, it now invokes the cammera app

--- app/src/main/java/com/example/laurencecuffeassignment2/MainActivity.java
+++ app/src/main/java/com/example/laurencecuffeassignment2/MainActivity.java
@@ -21,12 +21,14 @@ public class MainActivity extends AppCompatActivity {
     // defining a variable for the view object textView
     protected TextView camera_open_id;
     protected TextView picPic;
+    protected TextView mailGo;
     Uri imageUri;
 
     /**
      * @author Laurence Cuffe
-     * @version 0.8
+     * @version 0.94
      * Includes a working Implied intent for Open Camera trigering off the first textView
+     * Includes test code to triger photo off third button
      * includes a working Implied intent for View Picture, This code is very relient on an example
      * from https://www.tutorialspoint.com/how-to-pick-an-image-from-image-gallery-in-android
      * @see <a href="https://www.tutorialspoint.com/how-to-pick-an-image-from-image-gallery-in-android">https://www.tutorialspoint.com/how-to-pick-an-image-from-image-gallery-in-android</a>
@@ -37,17 +39,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         camera_open_id =  findViewById(R.id.textView);
-        picPic = findViewById(R.id.textView2);
-
         Log.i(PTAG,"Camera_open_Id set ");
+        picPic = findViewById(R.id.textView2);
+        Log.i(VTAG,"Gallery open picPic is set ");
+        mailGo = findViewById(R.id.textView3);
+
+
         picPic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openGallery();
             }
-
-
         });
+
+        mailGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
         camera_open_id.setOnClickListener(new View.OnClickListener() {
 
             @Override
